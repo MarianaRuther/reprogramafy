e9a40f72165ee9d813f03c688209c1d091263669
Mariana Ruther <marianaruther@gmail.com>
2017-10-20 16:01:47 -0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//login
 var botao = document.getElementById("botao");
 var fundo = document.getElementById("fundo");
 var fechar = document.getElementById("fechar");
@@ -19,33 +20,58 @@ fundo.onclick = function (event) {
     }
 }
 
+//seleciona o formulário
+var form = document.querySelector('form');
+//quando for enviar mandar dados
+form.onsubmit = function(e) {
 
 // document.querySelector('#botao') <--quando for classe, coloca .botao para chamar no js;
 // console.log(botao); <--testar o que tiver errado na class;
 
-
-//login
-
-var email = document.getElementById("email");
-
-
+var email = document.querySelector("#email");
 var senha = document.querySelector("#senha");
 
-var formulario = document.querySelector("form"); // vou informar que eu quero um formulário específico
-formulario.onsubmit = function () {
-    if (email.value !== "admin@admin.com" || senha.value !== "admin") {
-        alert("Email ou Senha errados");
-        modal.classList.add("erro");
+//seleciona a modal
+var modal = document.querySelector('#modal');
 
-        setTimeout(function () {
-            modal.classList.remove("erro");
-        }, 1000);  /*1000 = 1s */
-        return false;
+//verifica se o email e a senha são permitidos
+var usuarios = JSON.parse(localStorage.usuarios);
+var correto;
+for (var i = 0; i < usuarios.legth; i++) {
+    if (usuarios[i].email === email.value && usuarios[i].senha === senha.value) {
+        correto = true;
+        break;
     }
+}
 
-    localStorage.usuario = email.value;
+if(!correto) {
+    //adiciona classe de erro na modal para mostrar um feedback
+    modal.classList.add('erro');
+
+    //remove a classe com erro depois de trazer a animação
+    setTimeout(function() {
+        modal.classList.remove('erro');
+    }, 1000);
+}
+
+//não deixa o usuário enviar o formulários
+    return false;
 }
 
 
-// return false = vai para a página do action dentro do form;     
+// var formulario = document.querySelector("form"); // vou informar que eu quero um formulário específico
+// formulario.onsubmit = function () {
+//     if (email.value !== localStorage.email || senha.value !== localStorage.senha) {
+//         alert("Email ou Senha errados");
+//         modal.classList.add("erro");
+
+//         setTimeout(function () {
+//             modal.classList.remove("erro");
+//         }, 1000);  /*1000 = 1s */
+//         return false;
+//     }
+
+//     localStorage.usuario = email.value;
+// }
+// // return false = vai para a página do action dentro do form;     
 
